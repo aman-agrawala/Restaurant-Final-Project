d23b18ebeb6092729afb5b7cd20a17e5bbcbbe56
Aman Agrawala <amanagrawala@gmail.com>
2016-03-21 00:49:51 -0400

    feat: Add Fb login for authentication/authorization.

--- project.py
+++ project.py
@@ -195,7 +195,74 @@ def gdisconnect():
     response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
     response.headers['Content-Type'] = 'application/json'
     return response
- 
+
+@app.route('/fbconnect', methods = ['POST'])
+def fbconnect():
+
+  # First verify the value of state  to protect against cross site reference forgery attacks
+  if request.args.get('state') != login_session['state']:
+    response = make_response(json.dumps('Invalid state parameter.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+  # Now we exchange this short-lived token for the long-lived token here.
+  access_token = request.data
+  # Exchange client token for long-lived server-side token with GET 
+  # /oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret={app-secret}&fb_exchange_token={short-lived-token}
+  app_id = json.loads(open('fb_client_secrets.json','r').read())['web']['app_id']
+  app_secret = json.loads(open('fb_client_secrets.json','r').read())['web']['app_secret']
+  # note you have to send the app_secret along to Facebook to verify the server's identity
+  url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' \
+         % (app_id, app_secret, access_token)
+  h = httplib2.Http()
+  # This token includes an expires field that indicates how long this token is valid. Long term
+  # tokens can last up to two months. 
+  result = h.request(url, 'GET')[1]
+
+  # Use token to get user info from API
+  userinfo_url = "https://graph.facebook.com/v2.2/me"
+  #strip expire tag form access token since you don't need it to make API calls
+  token = result.split('&')[0]
+
+  # if token works then we should be able to make API calls with the new token as shown below in
+  # the next 3 lines
+  url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+  h = httplib2.Http()
+  result = h.request(url,'GET')[1]
+
+  # print "url sent for API access:%s" % url
+  # print "API JSON result: %s" % result
+  # Here we populate the login_session with the token provided.
+  data = json.loads(result)
+  login_session['provider'] = 'facebook'
+  login_session['username'] = data['name']
+  login_session['email'] = data['email']
+  login_session['facebook_id'] = data['id']
+
+  # Note facebook uses a separate API call to retrieve a profile picture.
+  # Get user picture
+  url = 'https://graph.facebook.com/v2.2/me/picture?%s&redirect=0&height=200&width=200' % token
+  h = httplib2.Http()
+  result = h.request(url,'GET')[1]
+  data = json.loads(result)
+
+  login_session['picture'] = data["data"]["url"]
+
+  # New we see if the user exists
+  user_id = getUserID(login_session['email'])
+  if not user_id:
+    user_id = createUser(login_session)
+  login_session['user_id'] = user_id
+
+  output = ''
+  output += '<h1>Welcome, '
+  output += login_session['username']
+
+  output += '!</h1>'
+  output += '<img src"'
+  output += login_session['picture']
+  output += ' " style = " width: 300px; height: 300px; border-radius: 150px; \
+                -webkit-border-radius: 150px; -moz-border-radius: 150px;"> '
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
